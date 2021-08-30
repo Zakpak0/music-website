c76ed85b53ae0e16182d66767abf88f1ed787867
LAPTOP-KO3E8U1N\18082 <ZakharyOliver808@gmail.com>
2021-08-29 14:24:29 -1000

    Update signin.tsx
    CSS updates

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -2,11 +2,11 @@ const signin = (): JSX.Element => {
     return (
             <div className="flex mx-10 flex-col flex-grow">
         <div>
-            <div className="flex justify-center py-8">
+            <div className="flex justify-center lg:py-8">
                 <div className="flex flex-col justify-center text-center">
                     <h1>Log in</h1>
-                            <div className="border w-80 h-80 p-5 mt-32">
-                                <div className="pt-10">
+                            <div className="border w-80 lg:h-80 p-5 lg:mt-32">
+                                <div className="lg:pt-10">
                                     <div>
                                         <h1 className="border">
                                             User Name
@@ -26,8 +26,8 @@ const signin = (): JSX.Element => {
                                         <input className="border text-center py-3" placeholder={"Password"}></input>
                                     </div>
                                 </div>
-                                <div className="border">
-                                        <button className="border text-center py-3">Log In</button>
+                                <div className="border mx-14 w-40">
+                                        <button className="border text-center w-40 py-3">Log In</button>
                                 </div>
                             </div>
                         </div>
